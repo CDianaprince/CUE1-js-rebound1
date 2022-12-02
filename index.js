126908c57ff29e1ede94e981346abd6abc2df7df
unknown <dcarol.prince@gmail.com>
2022-12-02 11:02:33 -0600

    js

---
+++ index.js
@@ -0,0 +1,30 @@
+//Obtener informacion de la forma
+
+const nombre = document.getElementById("nombre");
+
+const correo = document.getElementById("email");
+
+const form = document.querySelector5(".form1");
+
+//Crear event listener para la forma
+
+form.addEventListener('enviar', (e) => {
+  //Evitar la accion por defecto
+  e.preventDefault();
+
+  //Crear un objeto que tenga los valores de la forma
+
+  const formulario = {
+
+    name: nombre.value,
+    email: correo.value,
+
+  }
+
+  console.log(formulario);
+
+  //Dar la orden de submit 
+  // event.submit();
+
+});
+
